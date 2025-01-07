b51a7368c4a10031dfb626cfebf004947e238bc7
Euan Gilmour <euan.gilmour.2021@uni.strath.ac.uk>
2025-01-07 21:54:21 +0000

    Add KeyViewModel

---
+++ app/src/main/java/org/walletapp/KeyViewModel.kt
@@ -0,0 +1,44 @@
+package org.walletapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.walletapp.crypto.KeyManager
+
+class KeyViewModel : ViewModel() {
+
+    private var _keyStatus = androidx.compose.runtime.mutableStateOf("No Key")
+    val keyStatus = _keyStatus
+
+    private var _keySecurityLevel = androidx.compose.runtime.mutableStateOf("Unchecked")
+    val keySecurityLevel = _keySecurityLevel
+
+    private var _publicKey = androidx.compose.runtime.mutableStateOf("No Key")
+    val publicKey = _publicKey
+
+    fun checkKey() {
+        _keyStatus.value = if (KeyManager.keyExists()) "Key Exists" else "No Key"
+        _publicKey.value = KeyManager.getPublicKey().toString()
+    }
+
+    fun generateKey() {
+        viewModelScope.launch(Dispatchers.IO) {
+            KeyManager.generateKeys()
+            _keyStatus.value = "Key Generated"
+        }
+    }
+
+    fun deleteKey() {
+        viewModelScope.launch(Dispatchers.IO) {
+            KeyManager.deleteKey()
+            _keyStatus.value = "Key Deleted"
+            _publicKey.value = "No Key"
+        }
+    }
+
+    fun getSecurityLevel() {
+        _keySecurityLevel.value = KeyManager.getSecurityLevel()
+    }
+
+}
